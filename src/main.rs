c49b0e528e22d638bce095495a6cf15f7956bb74
Tiago Correia <tiagorcorreia87@gmail.com>
2022-12-27 12:56:22 +0000

    Changes in the algorithm and add some comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use std::io::{self, Write};
-use std::process::{self, Command};
+//! # rtodo 
+//!
+//! I wrote this project while I'm learning Rust for practice.
+//! I decided to put this project online so that others, like me, 
+//! who are learning Rust can take some ideas and help me improve this code too.
+//!
+//! Feel free to open a pull request to improve the code or to 
+//! add new features and help me learn Rust.
 
+// Read input and flushing
+use std::io::{self, Write};
+
+// For Command and Exit
+use std::process::{self,Command};
+
+// For colored output
 use colored::Colorize;
 
 use todos::Todo;
 
 pub mod todos;
 
+
+
+/// Get user input and return the value as a String
 fn user_input() -> Result<String, Box<dyn std::error::Error>> {
     let mut buffer = String::new();
     io::stdout().flush()?;
@@ -14,6 +30,7 @@ fn user_input() -> Result<String, Box<dyn std::error::Error>> {
     Ok(buffer)
 }
 
+/// Add todos into `Vec<Todo>`
 fn add_todo(td: &mut Vec<Todo>) -> Result<(), Box<dyn std::error::Error>> {
     print!("{}{}", "Title ".white().bold(), "-> ".green().bold());
     let title = user_input()?.trim().to_string();
@@ -26,7 +43,9 @@ fn add_todo(td: &mut Vec<Todo>) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+/// Show todos in formatted way
 fn show_todos(todos: &Vec<Todo>) {
+    // Clear the screen
     Command::new("clear").status().unwrap();
 
     println!(
@@ -36,6 +55,7 @@ fn show_todos(todos: &Vec<Todo>) {
         "Description".blue().bold()
     );
 
+    // Format the output with hyphens 
     println!(
         "{:-^10} {:-^40} {:-^40}",
         "".blue().bold(),
@@ -43,6 +63,7 @@ fn show_todos(todos: &Vec<Todo>) {
         "".blue().bold()
     );
 
+    // Check if todos are empty if not show them
     if !todos.is_empty() {
         for (i, x) in todos.iter().enumerate() {
             println!(
@@ -55,6 +76,8 @@ fn show_todos(todos: &Vec<Todo>) {
     }
 }
 
+
+/// remove todos from `Vec<Todo>` with a given ID
 fn remove_todo(todos: &mut Vec<Todo>) -> Result<(), Box<dyn std::error::Error>> {
     print!(
         "\n{} {}{}",
@@ -63,15 +86,20 @@ fn remove_todo(todos: &mut Vec<Todo>) -> Result<(), Box<dyn std::error::Error>> 
         "-> ".green().bold()
     );
 
+    // Get ID from user and parse it into usize
     let rm = user_input()?.trim().to_string().parse::<usize>()?;
 
+    // Check if `Vec<Todo>` is not empty and remove the Todo
     if rm < todos.len() && !todos.is_empty() {
         todos.remove(rm);
     };
+
     Ok(())
 }
 
+/// Update todos with a given ID 
 fn update_todo(todos: &mut [Todo]) -> Result<(), Box<dyn std::error::Error>> {
+
     print!(
         "\n{} {}{}",
         "ID".red().bold(),
@@ -79,17 +107,21 @@ fn update_todo(todos: &mut [Todo]) -> Result<(), Box<dyn std::error::Error>> {
         "-> ".green().bold()
     );
 
+    // Get ID from user
     let id = user_input()?.trim().to_string();
-
     print!("{}{}", "Title ".white().bold(), "-> ".green().bold());
 
+    // Get Title from user
     let title = user_input()?.trim().to_string();
-
     print!("{}{}", "Description ".white().bold(), "-> ".white().bold());
+
+    // Get Desc from user
     let desc = user_input()?.trim().to_string();
 
+    // Parsing ID String to usize if possible
     let id = id.trim().to_string().parse::<usize>()?;
 
+    // Find the ID and update with values given from user
     for (i, z) in todos.iter_mut().enumerate() {
         if i == id {
             z.title = title.to_string();
@@ -99,6 +131,7 @@ fn update_todo(todos: &mut [Todo]) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+/// Just print the Main Menu
 fn main_menu() {
     println!(
         "\n{:>5} {}\n{:>5} {}\n{:>5} {}\n{:>5} {}\n{:>5} {}\n",
@@ -115,6 +148,7 @@ fn main_menu() {
     );
 }
 
+/// Show a Sub Menu 
 fn sub_menu(todos: &mut Vec<Todo>) -> Result<bool, Box<dyn std::error::Error>> {
     print!(
         "\n\n{} {:<10} {} {:<10} {} {:<10}",
@@ -125,6 +159,7 @@ fn sub_menu(todos: &mut Vec<Todo>) -> Result<bool, Box<dyn std::error::Error>> {
         "[3]".blue().bold(),
         "Remove Todo -> ".white().bold(),
     );
+
 
     let id = user_input()?.trim().to_string();
 
@@ -143,34 +178,44 @@ fn sub_menu(todos: &mut Vec<Todo>) -> Result<bool, Box<dyn std::error::Error>> {
     }
 }
 
+/// Main function
 fn main() {
-    let mut todos: Vec<Todo> = vec![Todo {
-        title: "testes".to_string(),
-        description: "desctestes".to_string(),
-    }];
+    // Create an empty Vector to old the Todos
+    let mut todos: Vec<Todo> = vec![];
 
+    // Main loop
     loop {
+        // Clear the Screen
         Command::new("clear").status().unwrap();
+
+        // Show Main Menu
         main_menu();
 
+        // Let user choose an option
         print!(
             "{}{}",
             "Choose an option ".white().bold(),
             "-> ".green().bold()
         );
 
+        // Check if input from user is valid
         let user = match user_input() {
             Ok(u) => u.trim().to_string(),
             Err(e) => panic!("Error get user input {e}"),
         };
 
+
         if user.contains('1') {
             show_todos(&todos);
 
+            // A Loop with submenu
             while let Ok(e) = sub_menu(&mut todos) {
+                // if the value returned by sub menu is
+                // false then break the loop
                 if !e {
                     break;
                 }
+                
                 show_todos(&todos);
             }
         } else if user.contains('2') {
@@ -190,5 +235,5 @@ fn main() {
         } else if user.contains('5') {
             process::exit(0);
         }
-    }
+    }// end main Loop
 }
--- src/todos.rs
+++ src/todos.rs
@@ -1,10 +1,11 @@
+/// Struct to keep the todos
 pub struct Todo{
    pub title: String,
    pub description: String,
 }
 
 impl Todo{
-
+    // Constructor for initializing the Todo
     pub fn new(title:String,description:String) -> Self{
         Self{
             title,
